f801d114c78e22c90975e4a1926e908d233aabe3
sladesha <stw386@sina.com>
2019-07-29 10:05:09 +0800

    change code structure

--- PyTls/Chinese2num.py
+++ PyTls/Chinese2num.py
@@ -8,8 +8,7 @@
 '''
 
 from .typet import is_type, is_none
-from math import log
-from math import e
+
 
 _zh_num = {'壹': 1, '贰': 2, '叁': 3, '肆': 4, '伍': 5, '陆': 6, '柒': 7, '捌': 8, '玖': 9, '拾': 10, '零': 0, '一': 1, '二': 2,
            '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9, '十': 10, "两": 2}
@@ -24,10 +23,6 @@ def Chinese_2_num(ch):
         return _zh_num.get(ch)
     else:
         return ''.join(map(str, [_zh_num.get(x) for x in ch if not is_none(_zh_num.get(x))]))
-
-
-def ln(num):
-    return log(num, e)
 
 
 def isdigit(num):
