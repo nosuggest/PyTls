bdab26bce0250867045627a09084a4f34f975309
shataowei <taowei.sha@ymm56.com>
2019-10-30 23:06:35 +0800

    修复bug/新增计时功能/新增json加载功能

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyTls',
-    version='0.0.7',
+    version='0.0.8',
     description=(
         'python tools, we need something fast and easy'
     ),
